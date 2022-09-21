38f40dd105a8244d9223f319f8348e26ccd19798
Kuku Ruyuk <kuukuuuruyuk@gmail.com>
2022-09-21 18:45:09 +0800

    create migrate usr album likes

---
+++ migrations/1663756784113_user-album-likes.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('user_album_likes', {
+    id: {
+      type: 'varchar(60)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'text',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'cascade',
+    },
+    album_id: {
+      type: 'text',
+      notNull: true,
+      references: '"albums"',
+      onDelete: 'cascade',
+    },
+  });
+
+  pgm.createIndex('user_album_likes', ['user_id', 'album_id']);
+};
+
+exports.down = (pgm) => {
+  pgm.dropIndex('user_album_likes', ['user_id', 'album_id']);
+  pgm.dropTable('user_album_likes');
+};
